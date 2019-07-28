e25e3d187d24c86bc4ed01fe6e841d9a58eccf90
Federico Armellini <armellini.federico@gmail.com>
2019-07-28 13:53:56 +0200

    2019/2020

--- LinkValidator.py
+++ LinkValidator.py
@@ -10,16 +10,24 @@ def validate_link(link):
     return "tgme_page_title" in requests.get(link).text
 
 
-data = get_db_as_json()
-for p in data['info_data']:
-    if not ("FB" in p) and ("TG" in p):
-        pvt_link_format = "https://t.me/joinchat/"
-        pvt_link_format += p.split("/")[len(p.split("/")) - 1]
+def main(yearSelected):
+    separator = ' |-| '
 
-        pbl_link_format = "https://t.me/"
-        pbl_link_format += p.split("/")[len(p.split("/")) - 1]
+    data = get_db_as_json()
+    for p in data['info_data']:
+        if not ("FB" in p) and ("TG" in p):
 
-        if validate_link(pvt_link_format) is False and validate_link(pbl_link_format) is False:
-            class_name = data['info_data'][p]['class']
-            print(f"Link of {class_name} not working anymore. You're pleased to update it. "
-                  f"({pvt_link_format} or {pbl_link_format}")
+            year = data['info_data'][p]['year']
+            if year is None or year == "" or year == yearSelected:
+                pvt_link_format = "https://t.me/joinchat/"
+                pvt_link_format += p.split("/")[len(p.split("/")) - 1]
+
+                pbl_link_format = "https://t.me/"
+                pbl_link_format += p.split("/")[len(p.split("/")) - 1]
+
+                if validate_link(pvt_link_format) is False and validate_link(pbl_link_format) is False:
+                    class_name = data['info_data'][p]['class']
+                    print(f"{class_name}{separator}{pvt_link_format}{separator}{pbl_link_format}{separator}{year}")
+
+
+main("2019/2020")
